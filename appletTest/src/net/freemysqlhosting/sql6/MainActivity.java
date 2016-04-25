1753315f69346036ccd050a29860a80b96078e9f
stirlitztrojan <stirlitztrojan@gmail.com>
2016-04-26 01:25:13 +0300

    Added files via upload

---
+++ appletTest/src/net/freemysqlhosting/sql6/MainActivity.java
@@ -0,0 +1,10 @@
+
+package net.freemysqlhosting.sql6;
+
+public class MainActivity {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
---
+++ appletTest/src/net/freemysqlhosting/sql6/MainApplet.java
@@ -0,0 +1,11 @@
+package net.freemysqlhosting.sql6;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class MainApplet extends Applet {
+    @Override
+    public void paint(Graphics g) {
+        g.drawString("This is a test", 20, 20);
+    }
+}
